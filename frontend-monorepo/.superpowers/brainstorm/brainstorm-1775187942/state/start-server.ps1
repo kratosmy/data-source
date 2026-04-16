4e579f807a6dbde2d397fbc3380557dcab70da35
kratosmy <kratosmy@proton.me>
2026-04-16 12:03:59 +0800

    fix: remove unused config files

--- frontend-monorepo/.superpowers/brainstorm/brainstorm-1775187942/state/start-server.ps1
+++
@@ -1,6 +0,0 @@
-$env:BRAINSTORM_DIR = 'D:\projects\frontend-monorepo\.superpowers\brainstorm\brainstorm-1775187942'
-$env:BRAINSTORM_HOST = '127.0.0.1'
-$env:BRAINSTORM_URL_HOST = 'localhost'
-Remove-Item Env:BRAINSTORM_OWNER_PID -ErrorAction SilentlyContinue
-Set-Location 'C:\Users\lov3c\.codex\superpowers\skills\brainstorming\scripts'
-node 'C:\Users\lov3c\.codex\superpowers\skills\brainstorming\scripts\server.cjs'
